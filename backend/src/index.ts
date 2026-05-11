064015c612f4253882bc13cbe780f022676976a4
Devansh Modi <devanshmodi250@gmail.com>
2026-05-12 01:00:22 +0530

    deployement bug

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-  app.get('*', (_, res) => {
+  app.get('/{*path}', (_, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
   });
 }
