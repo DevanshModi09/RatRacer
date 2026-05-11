8cfe756071cd2c033ce52efbace27e1945b88ff0
Devansh Modi <devanshmodi250@gmail.com>
2026-05-11 23:18:26 +0530

    trying to create frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,37 @@
-import { useState, useRef } from 'react';
-import { connectWs } from './utils/connectWs.ts';
-import TypingArea from './components/TypingArea.tsx';
-const App = () => {
-  
-};
+import { useState, useRef, type ReactEventHandler, useEffect } from 'react';
+import { Socket } from 'socket.io-client';
+import { socket } from './utils/socket';
 
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [roomCode, setRoomCode] = useState('');
+  const [isReady, setIsReady] = useState('');
+  useEffect(() => {
+    // socket.on();
+  }, []);
+  const handleCreateRoom = () => {};
+  return (
+    <>
+      <div>
+        <label htmlFor="usernameInput">Enter The Username</label>
+        <input
+          name="usernameInput"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        ></input>
+      </div>
+
+      {username ? (
+        <div>
+          {' '}
+          <button onClick={handleCreateRoom}>CreateRoom</button>
+        </div>
+      ) : (
+        <div>Enter Username To Create Room</div>
+      )}
+    </>
+  );
+};
 export default App;
--- frontend/src/utils/connectWs.ts
+++
@@ -1,6 +0,0 @@
-import { io } from 'socket.io-client';
-
-export const connectWs = () => {
-  const socket = io('http://localhost:3000');
-  return socket;
-};
