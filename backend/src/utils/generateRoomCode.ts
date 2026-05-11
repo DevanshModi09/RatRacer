bda3ff13ad5082ab86c1a449415401b1d1325144
Devansh Modi <devanshmodi250@gmail.com>
2026-05-11 19:13:34 +0530

    raceEvents??

--- backend/src/utils/generateRoomCode.ts
+++ backend/src/utils/generateRoomCode.ts
@@ -1,3 +1,3 @@
 export function generateRoomCode(): string {
-  return Math.random().toString(36).substring(2, 8).toUpperCase();
+  return Math.random().toString(36).substring(2, 12).toUpperCase();
 }
